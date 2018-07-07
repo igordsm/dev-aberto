6e1f9b64768e065e1cf5adfdbd8f5990835217be
Igor <igordsm@gmail.com>
2018-07-07 17:37:26 -0300

    Início da exibição de skills na página da disciplina.

--- update_website.py
+++ update_website.py
@@ -8,10 +8,19 @@ if __name__ == "__main__":
     with open('players/player_list.json') as pfp:
         players = json.load(pfp)
     
+    with open('skills/skill_list.json') as pfp:
+        skills = json.load(pfp)
+
     for plr in players:
         plr["raw_img"] = 'https://github.com/igordsm/dev-aberto/raw/master/players/avatar_data/' + plr["avatar_image"]
         plr["xp"] = 0
         plr["level_name"] = "Newbie"
+
+        # parse skill list and achievements
+        plr["skills"] = []
+        for sk in skills:
+            plr["skills"].append(sk.copy())
+            plr["skills"][-1]["achieved"] = False
 
     out = t.render(players=players)
 
