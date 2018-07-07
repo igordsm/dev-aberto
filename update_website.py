5f890ebc8e306ff1fb1cdb268afe338a5843668b
Igor <igordsm@gmail.com>
2018-07-07 18:54:39 -0300

    Começo do código para mostrar as skills no site.

--- update_website.py
+++ update_website.py
@@ -10,17 +10,22 @@ if __name__ == "__main__":
     
     with open('skills/skill_list.json') as pfp:
         skills = json.load(pfp)
+        for sk in skills:
+            sk["raw_img"] = "https://github.com/igordsm/dev-aberto/raw/master/skills/images/" + sk["badge"]
+
+    # parse achievement list and set proof and achieved flags.
 
     for plr in players:
         plr["raw_img"] = 'https://github.com/igordsm/dev-aberto/raw/master/players/avatar_data/' + plr["avatar_image"]
         plr["xp"] = 0
         plr["level_name"] = "Newbie"
 
-        # parse skill list and achievements
         plr["skills"] = []
         for sk in skills:
             plr["skills"].append(sk.copy())
             plr["skills"][-1]["achieved"] = False
+        
+        
 
     out = t.render(players=players)
 
